82d300090eed2599e14dae680cb6cc40b4601ada
Lex Kolesnikov <kyposani@outlook.com>
2020-06-21 20:54:23 +0300

    L1 completed

---
+++ chapters/18_Reflection/L_01-Intro/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace L_01_Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type t = typeof(User);
+
+            User user = new User("Tom", 30);
+            Type myType = user.GetType();
+
+            Type getT = Type.GetType("L_01_Intro.User", false, true);
+
+            Console.WriteLine(myType);
+            Console.WriteLine(t);
+            Console.WriteLine(getT);
+        }
+    }
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public User(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Имя: {Name}  Возраст: {Age}");
+        }
+        public int Payment(int hours, int perhour)
+        {
+            return hours * perhour;
+        }
+    }
+}
