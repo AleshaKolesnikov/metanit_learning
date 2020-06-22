0c9dce3fcf154fdd9bd315e620d825a3c68210bd
Lex Kolesnikov <kyposani@outlook.com>
2020-06-22 17:31:17 +0300

    L1 is completed

---
+++ chapters/25_Validation/L_01_Basics/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace L_01_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Введите возраст");
+            int age = Int32.Parse(Console.ReadLine());
+
+            User user = new User { Name = name, Age = age };
+
+            List<ValidationResult> results = new List<ValidationResult>();
+            ValidationContext context = new ValidationContext(user);
+
+            if(!Validator.TryValidateObject(user, context, results, true))
+            {
+                foreach (var error in results)
+                {
+                    Console.WriteLine(error.ErrorMessage);
+                }
+            }
+        }
+    }
+}
---
+++ chapters/25_Validation/L_01_Basics/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace L_01_Basics
+{
+    public class User
+    {
+        [Required]
+        public string Id { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+        [Required]
+        [Range(1,100)]
+        public int Age { get; set; }
+    }
+}
