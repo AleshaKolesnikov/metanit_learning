60fa2028cf48aebc98127cefc43fbab0b09b09a5
Lex Kolesnikov <kyposani@outlook.com>
2020-06-22 16:35:03 +0300

    L3 is completed

---
+++ chapters/18_Reflection/L_03-assembly-practice/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reflection;
+
+namespace L_03_assembly_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+				Assembly asm = Assembly.LoadFrom(@"C:\Users\kypos\Desktop\metanit\metanit_learning\chapters\18_Reflection\L_03-assembly\bin\Debug\netcoreapp3.1\L_03-assembly.dll");
+
+				Type t = asm.GetType(@"L_03_assembly.Program", true, true);
+
+				object instance = Activator.CreateInstance(t);
+
+				MethodInfo method = t.GetMethod("GetResult");
+
+				object result = method.Invoke(instance, new object[] { 6, 100, 3 });
+				Console.WriteLine(result);
+
+				Console.WriteLine("Вызов метода main");
+
+				Assembly assm = Assembly.LoadFrom(@"C:\Users\kypos\Desktop\metanit\metanit_learning\chapters\18_Reflection\L_03-assembly\bin\Debug\netcoreapp3.1\L_03-assembly.dll");
+				Type t2 = assm.GetType(@"L_03_assembly.Program", true, true);
+
+				object programInst = Activator.CreateInstance(t2);
+				MethodInfo mainMethod = t2.GetMethod("Main", BindingFlags.NonPublic | BindingFlags.Static);
+
+				object res = mainMethod.Invoke(programInst, new object[] { new string[] { } });
+				Console.WriteLine(res);
+
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+        }
+    }
+}
---
+++ chapters/18_Reflection/L_03-assembly/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+
+namespace L_03_assembly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Assembly assm = Assembly.LoadFrom("L_03-assembly.dll");
+            //Assembly assm = Assembly.Load("L_03-assembly");
+
+            //Console.WriteLine(assm.FullName);
+
+            //Type[] types = assm.GetTypes();
+            //foreach (Type t in types)
+            //{
+            //    Console.WriteLine(t.Name);
+            //}
+
+            Console.WriteLine(GetResult(6, 100, 2));
+
+        }
+        public static double GetResult(int percent, double capital, int year)
+        {
+            for (int i = 0; i < year; i++)
+            {
+                capital += capital / 100 * percent;
+            }
+            return capital;
+        }
+    }
+}
