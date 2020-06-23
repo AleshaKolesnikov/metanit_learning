a5041db949b506c3f0565632e552a9100d424234
Lex Kolesnikov <kyposani@outlook.com>
2020-06-23 15:13:34 +0300

    L2 is completed

---
+++ chapters/25_Validation/L_02_validation_attributes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace L_02_validation_attributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите Id:");
+            string id = Console.ReadLine();
+
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Введите возраст");
+            int age = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите пароль:");
+            string password = Console.ReadLine();
+
+            Console.WriteLine("Введите пароль повторно");
+            string confirm = Console.ReadLine();
+
+            User user = new User { Age = age, Id = id, Name = name, Password = password, ConfirmPassword = confirm };
+
+            ValidateUser(user);
+        }
+
+        static void ValidateUser(object obj)
+        {
+            var results = new List<ValidationResult>();
+            var context = new ValidationContext(obj);
+
+            if (!Validator.TryValidateObject(obj, context, results, true))
+            {
+                foreach (var error in results)
+                {
+                    Console.WriteLine(error.ErrorMessage);
+                }
+            }
+        }
+    }
+}
---
+++ chapters/25_Validation/L_02_validation_attributes/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace L_02_validation_attributes
+{
+    public class User
+    {
+        [Required(ErrorMessage = "Идентификатор пользователя не установлен")]
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "Не указано имя пользователя")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Недопустимая длина имени")]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1, 100, ErrorMessage = "недопустимый возраст")]
+        public int Age { get; set; }
+
+        
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "не соответствует")]
+        public string ConfirmPassword { get; set; }
+    }
+}
