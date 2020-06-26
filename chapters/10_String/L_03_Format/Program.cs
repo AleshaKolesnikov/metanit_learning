41a2bc74617ed4125d91ec4499789a5a99d09644
Lex Kolesnikov <kyposani@outlook.com>
2020-06-26 15:36:51 +0300

    L3 is completed

---
+++ chapters/10_String/L_03_Format/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace L_03_Format
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person tom = new Person { Age = 18, Name = "Tom" };
+
+            //Console.WriteLine("Name: {0}, Age - {1}", tom.Name, tom.Age);
+
+            //string format = String.Format("Name: {0}, Age - {1}", tom.Name, tom.Age);
+            //Console.WriteLine(format);
+
+            //double number = 23.7;
+            //int number = 23;
+            long number = 375447250000;
+
+            //string result = String.Format("{0:C}", number);
+            //string result = String.Format("{0:d}", number);
+            //string result = String.Format("{0:f5}", number);
+            //string result = String.Format("{0:p}", number);
+            string result = String.Format("{0:+###-(##) ###-##-##}", number);
+            //Console.WriteLine(number.ToString("+###-(##) ###-##-##"));
+            Console.WriteLine(result);
+        }
+    }
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
