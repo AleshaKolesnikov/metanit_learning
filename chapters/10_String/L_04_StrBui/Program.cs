543a3c8b32107e3aa36fa7b2e4c415b1eab3b440
Lex Kolesnikov <kyposani@outlook.com>
2020-06-26 15:51:13 +0300

    L_4 is completed

---
+++ chapters/10_String/L_04_StrBui/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+
+namespace L_04_StrBui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //StringBuilder sb = new StringBuilder("Some string");
+            //Console.WriteLine(sb.Length);
+            //Console.WriteLine(sb.Capacity);
+
+            //StringBuilder sb = new StringBuilder("Название: ");
+            //Console.WriteLine(sb);
+            //Console.Write($"Длина строки: {sb.Length}, ");
+            //Console.WriteLine($"Емкость строки: {sb.Capacity}");
+
+            //sb.Append("Руководство");
+            //Console.WriteLine(sb);
+            //Console.Write($"Длина строки: {sb.Length}, ");
+            //Console.WriteLine($"Емкость строки: {sb.Capacity}");
+
+            //sb.Append(" по C#");
+            //Console.WriteLine(sb);
+            //Console.Write($"Длина строки: {sb.Length}, ");
+            //Console.WriteLine($"Емкость строки: {sb.Capacity}");
+
+            StringBuilder sb = new StringBuilder("Привет мир");
+            sb.Append("!");
+            sb.Insert(7, "компьютерный ");
+            Console.WriteLine(sb);
+
+            sb.Replace("мир", "world");
+            Console.WriteLine(sb);
+
+            sb.Remove(7, 13);
+            Console.WriteLine(sb);
+
+            string s = sb.ToString();
+            Console.WriteLine(s);
+        }
+    }
+}
