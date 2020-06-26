8837372fb3e949578b7fc40717846c2c379a64bb
Lex Kolesnikov <kyposani@outlook.com>
2020-06-26 17:19:50 +0300

    L1 is completed

---
+++ chapters/09_Collections/L_01_ArrayList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+namespace L_01_ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            list.Add(2.3);
+            list.Add(55);
+            list.AddRange(new string[] { "Hi", "man" });
+
+            foreach (object obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+            list.RemoveAt(0);
+            list.Reverse();
+            Console.WriteLine(list[0]);
+            foreach (var i in list)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ chapters/09_Collections/L_01_ArrayLisy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L_01_ArrayLisy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
